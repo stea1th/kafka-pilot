e441e674902bd1fc6ae39f572bec6e1e5fae4504
vlasov <stea1th@mail.ru>
2020-01-10 15:33:52 +0100

    kafka-pilot-test init

---
+++ kafka-pilot-test/src/main/java/de/stea1th/kafka/pilot/test/KafkaPilotTestApplication.java
@@ -0,0 +1,13 @@
+package de.stea1th.kafka.pilot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPilotTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPilotTestApplication.class, args);
+    }
+
+}
---
+++ kafka-pilot-test/src/test/java/de/stea1th/kafka/pilot/test/KafkaPilotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stea1th.kafka.pilot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPilotTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
