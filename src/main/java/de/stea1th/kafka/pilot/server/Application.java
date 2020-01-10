7e892f71d118ba00127bf0169a893b06c6e7f101
vlasov <stea1th@mail.ru>
2020-01-10 15:06:37 +0100

    application props

---
+++ src/main/java/de/stea1th/kafka/pilot/server/Application.java
@@ -0,0 +1,11 @@
+package de.stea1th.kafka.pilot.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
