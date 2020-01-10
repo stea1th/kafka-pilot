4b4138acd9a72d901438d2dcd9bb685a5972d58a
vlasov <stea1th@mail.ru>
2020-01-10 17:50:17 +0100

    fix

--- kafka-pilot-test/src/main/java/de/stea1th/kafka/pilot/test/service/ScheduledService.java
+++ kafka-pilot-test/src/main/java/de/stea1th/kafka/pilot/test/service/ScheduledService.java
@@ -1,18 +1,18 @@
-package de.stea1th.kafka.pilot.test;
+package de.stea1th.kafka.pilot.test.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.stea1th.kafka.pilot.test.dto.PizzaDto;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.Random;
 
 @Service
 @EnableScheduling
@@ -23,12 +23,10 @@ public class ScheduledService {
 
     private final ObjectMapper objectMapper;
 
-    private final BigDecimal bigDecimal;
 
     public ScheduledService(KafkaTemplate<Long, PizzaDto> kafkaPizzaTemplate, ObjectMapper objectMapper) {
         this.kafkaPizzaTemplate = kafkaPizzaTemplate;
         this.objectMapper = objectMapper;
-        this.bigDecimal = BigDecimal.valueOf(5.99);
     }
 
     @Scheduled(initialDelay = 5000, fixedDelay = 5000)
@@ -47,9 +45,13 @@ public class ScheduledService {
     private PizzaDto createDto() {
         var pizzaDto = new PizzaDto();
         pizzaDto.setName("Pizza Mozarella");
-        pizzaDto.setPrice(bigDecimal.add(BigDecimal.valueOf(0.5)).doubleValue());
+        pizzaDto.setPrice(getRandomPrice());
         return pizzaDto;
     }
 
-
+    private double getRandomPrice() {
+        var random = new Random();
+        var bigDecimal = new BigDecimal(random.nextDouble() * 100);
+        return bigDecimal.setScale(2, RoundingMode.CEILING).doubleValue();
+    }
 }
